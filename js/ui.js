972245000ff3263c8d5ae0b99fad8329c2021799
tafkap6969-create <tafkap6969@gmail.com>
2026-02-07 20:53:39 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,54 @@
 function renderHands() {
-  const playerHand = document.getElementById('player-hand');
-  const opponentHand = document.getElementById('opponent-hand');
+  renderPlayerHand();
+  renderOpponentHand();
+  renderTable();
 
-  playerHand.innerHTML = '';
-  opponentHand.innerHTML = '';
+  document.getElementById('trump').innerText =
+    'Козырь: ' + gameState.trump.value + gameState.trump.suit;
+}
 
-  gameState.players.player.forEach(card => {
+function renderPlayerHand() {
+  const hand = document.getElementById('player-hand');
+  hand.innerHTML = '';
+
+  gameState.players.player.forEach((card, index) => {
     const el = document.createElement('div');
     el.className = 'card';
     el.innerText = card.value + card.suit;
-    playerHand.appendChild(el);
+
+    el.onclick = () => toggleSelect(index, el);
+
+    if (card.selected) el.classList.add('selected');
+
+    hand.appendChild(el);
   });
+}
+
+function renderOpponentHand() {
+  const hand = document.getElementById('opponent-hand');
+  hand.innerHTML = '';
 
   gameState.players.opponent.forEach(() => {
     const el = document.createElement('div');
     el.className = 'card back';
-    opponentHand.appendChild(el);
+    hand.appendChild(el);
   });
+}
 
-  document.getElementById('trump').innerText =
-    'Козырь: ' + gameState.trump.value + gameState.trump.suit;
+function renderTable() {
+  const battle = document.getElementById('battle');
+  battle.innerHTML = '';
+
+  gameState.table.forEach(card => {
+    const el = document.createElement('div');
+    el.className = 'card';
+    el.innerText = card.value + card.suit;
+    battle.appendChild(el);
+  });
+}
+
+function toggleSelect(index) {
+  const card = gameState.players.player[index];
+  card.selected = !card.selected;
+  renderPlayerHand();
 }
