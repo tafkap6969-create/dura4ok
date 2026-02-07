6b116f7fb9d1f1363c0fbfe323705bec35c5bf9b
tafkap6969-create <tafkap6969@gmail.com>
2026-02-07 18:22:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Game started');
+const tg = window.Telegram.WebApp;
+tg.ready();
+tg.expand();
+
+const user = tg.initDataUnsafe?.user;
+
+if (user) {
+  document.getElementById('user').innerText =
+    `Привет, ${user.first_name} 👋`;
+}
+
+document.getElementById('play').onclick = () => {
+  tg.showAlert("Скоро начнём играть 🎴");
+};
